a40b847cef7817d5b026f44d25595a1322f6a405
Mmy2000 <yahianada005@gmail.com>
2024-04-17 10:03:48 +0200

    starting the project

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
+## Import Main PyQt Modules
+
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 import sys
+
+app = QApplication(sys.argv)
+window = QWidget()
+window.show()
+
+## Main Loop
+app.exec_()
